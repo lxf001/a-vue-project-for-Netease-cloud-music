336783789d507d6b1e89f1c9e77d3f4610235304
lxf <233@gmail.com>
2017-10-23 18:03:11 +0800

    museicon:zap:

--- src/pages/music.vue
+++ src/pages/music.vue
@@ -1,40 +1,24 @@
 <template>
-  <div class="wrapper">
-    <!--<el-carousel trigger="click" indicator-position="none" height="4.8rem">-->
-      <!--<el-carousel-item v-for="item in bannerData" :key="item.id">-->
-        <!--<a v-lazy="item.url">-->
-          <!--<img class="banner-pic" :src="item.pic" alt="">-->
-        <!--</a>-->
-      <!--</el-carousel-item>-->
-    <!--</el-carousel>-->
-  </div>
+<mu-paper>
+  <mu-bottom-nav :value="bottomNav" @change="handleChange">
+    <mu-bottom-nav-item value="recents" title="Recents" icon="restore"/>
+    <mu-bottom-nav-item value="favorites" title="Favorites" icon="favorite"/>
+    <mu-bottom-nav-item value="nearby" title="Nearby" icon="location_on"/>
+  </mu-bottom-nav>
+</mu-paper>
 </template>
+
 <script>
-  import {getBanner} from 'config/fetch'
-
-  export default {
-    data() {
-      return {
-        bannerData: []
-      }
-    },
-    mounted() {
-      this.init();
-    },
-    methods: {
-      async init() {
-        let res = await getBanner();
-        this.bannerData = res.code === 200 ? res.banners : []
-      }
-    },
-    computed: {}
+export default {
+  data () {
+    return {
+      bottomNav: 'recents'
+    }
+  },
+  methods: {
+    handleChange (val) {
+      this.bottomNav = val
+    }
   }
+}
 </script>
-<style lang="scss" >
-  @import '../assets/style/mixin.scss';
-
-  .banner-pic {
-    @include wh(100%, 4.8rem)
-  }
-
-</style>
